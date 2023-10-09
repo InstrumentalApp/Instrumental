b3ab3dd44b345565cca7625da4809a0ece94f2e2
japhethbear <japhethbear@gmail.com>
2023-10-09 00:07:05 -0500

    Added Footer Component. Gave simple stylings. Added Assets Folder With Images Folder Inside to Hold Image for Logo

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import axios from 'axios';
 import NavBar from './Components/NavBar';
 import Login from './Components/Login';
+import Footer from './Components/Footer'
 import { Route, Routes } from 'react-router-dom';
 import Example from './Components/Example';
 
@@ -27,6 +28,7 @@ function App() {
         <Route path="/hello" element={<Example />} />
         <Route path="/sign-in" element={<Login />} />
       </Routes>
+      <Footer/>
     </>
   )
 }
