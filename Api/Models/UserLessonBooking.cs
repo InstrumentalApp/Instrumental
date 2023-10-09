885d44f22042349e4469c53cb1b696719d216789
Brent Cleary <brent.cleary@gmail.com>
2023-10-08 19:46:26 -0700

    Student Field Dropped - User Field updated in Bookings and Seed

--- Api/Models/UserLessonBooking.cs
+++ Api/Models/UserLessonBooking.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace TeamFive.Models;
-public class StudentLessonBooking : BaseEntity
+public class UserLessonBooking : BaseEntity
 {
     // Date of Booking for Lesson
     [Required]
@@ -17,16 +17,16 @@ public class StudentLessonBooking : BaseEntity
     public int LessonId { get; set; }
     public Lesson? BookingLesson { get; set; }
     
-    public int StudentId { get; set; }
-    public Student? BookingStudent { get; set; }
+    public int UserId { get; set; }
+    public User? BookingUser { get; set; }
 
 
 }
 
 
 // Notes
-  // Students Book Lessons
-    // One Lesson can have many students
-    // One Student can have many lessons
+  // User's Book Lessons
+    // One Lesson can have many Users
+    // One User can have many lessons
 
   // Instructor Information is linked through Lesson Navigation Properties
