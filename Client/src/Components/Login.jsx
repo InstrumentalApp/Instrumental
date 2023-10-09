20ffc29094960b419a30cbaff07e1dbf6e773d08
shu-william <williamshu@outlook.com>
2023-10-08 23:06:15 -0700

    added material UI components for login form with links to register

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import useLocalStorage from '../Hooks/useLocalStorage';
-import { useNavigate } from 'react-router-dom';
-import axios from "axios"
+import { useNavigate, Link as RouterLink } from 'react-router-dom';
+import axios from 'axios';
+import '../Styles/LoginReg.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
 
 const Login = () => {
   const [errors, setErrors] = useState([]);
@@ -22,6 +30,7 @@ const Login = () => {
     try {
       const result = await axios.post("/api/auth/login", loginUser)
       setCredentials(result.data);
+      navigate('/hello');
     } catch (err) {
       const errorData = err.response.data.errors || err.response.data
       const errorMessages = Object.values(errorData).flat();
@@ -30,29 +39,74 @@ const Login = () => {
   };
 
   return (
-    <>
-      <h1>Login</h1>
-      {
-        errors.map((v, i) => <p key={i}>{v}</p>)
-      }
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          value={loginUser.email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={loginUser.password}
-          onChange={handleChange}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <h1>Sign in</h1>
+        {
+          errors.map((v, i) => <p key={i} className="errorStyle">{v}</p>)
+        }
+        <Box component="form" onSubmit={handleSubmit} 
+          sx={{
+            mt: 2, 
+            display: "flex", 
+            flexWrap: "wrap", 
+            justifyContent: "center"
+          }}>
+          <TextField
+            className="mb-3"
+            required
+            fullWidth
+            label="Email"
+            type="text"
+            name="email"
+            placeholder="Email"
+            autoFocus
+            value={loginUser.email}
+            onChange={handleChange}
+          />
+          <TextField
+            className="mb-3"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={loginUser.password}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              my: 2
+            }}
+          >
+            Sign In
+          </Button>
+          <Grid container direction="column" alignItems="center">
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link component={RouterLink} to={"/register"} variant="body2">
+                Register as a new user
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
