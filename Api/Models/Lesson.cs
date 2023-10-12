05e1283b16dd3fc9096991c25b6a961c1c039040
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2023-10-12 13:01:19 +0900

    Update Lesson.cs
    Applied Validations to Lesson Model
    Removed Default Duration

--- Api/Models/Lesson.cs
+++ Api/Models/Lesson.cs
@@ -1,23 +1,29 @@
 #pragma warning disable 8618
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using TeamFive.Attributes;
 
 namespace TeamFive.Models;
 public class Lesson : BaseEntity
 {
     [Key]
     public int LessonId { get; set; }
+    [FutureDate(DaysInAdvance = 2)]
     public DateTime BookingDate { get; set; }
-    public int DurationMinutes { get; set; } = 30;
+    [Range(15, 90, ErrorMessage = "Select a duration between 15 and 90 minutes")]
+    public int DurationMinutes { get; set; }
 
     // Associations
+    [Required]
     public int TeacherId { get; set; }
     public User? Teacher { get; set; }
 
     //Student is nullable
+    [Required]
     public int StudentId { get; set; }
     public User? Student { get; set; }
 
+    [Required]
     public int InstrumentId { get; set; }
     public Instrument? Instrument { get; set; }
 }
