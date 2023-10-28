47f455bcccf05b3c6cf0c8648f08109db883782d
Nicholas Ung <nicholasung22@gmail.com>
2023-10-28 10:23:13 -0700

    Update footer and lessons page

--- Client/src/Components/InstrumentLessonsOffered.jsx
+++ Client/src/Components/InstrumentLessonsOffered.jsx
@@ -2,7 +2,20 @@ import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import "../Styles/InstrumentLessons.css";
+<<<<<<< Updated upstream
 import { TextField } from "@mui/material";
+=======
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import styles from "../Styles/App";
+import Skeleton from '@mui/material/Skeleton';
+>>>>>>> Stashed changes
 
 const InstrumentLessonsOffered = () => {
   // API Call to Back-End When Data Gets Updated
@@ -53,6 +66,7 @@ const InstrumentLessonsOffered = () => {
         />
       </div>
       <div className="instrument-cards">
+<<<<<<< Updated upstream
         {instrumentList.map((instrument, index) => (
           <div key={index} className="instrument-card">
             {/* Not sure if we want to keep links routing to IDs or to instrument names */}
@@ -65,6 +79,58 @@ const InstrumentLessonsOffered = () => {
               <h6>{instrument.name}</h6>
             </Link>
           </div>
+=======
+        {loading ?
+        placeholders.map((index) => (
+          <Card key={index} elevation={0} className="instrument-card" sx={{ 
+          maxWidth: 345, 
+          backgroundColor: styles.colors.SECONDARY, 
+          paddingBottom: 2,
+          }}>
+            <Skeleton sx={{ height: 160, backgroundColor: styles.colors.FORM }} animation="wave" variant="rectangular"/>
+            <CardContent>
+              <div className="d-flex flex-column align-items-center gap-2">
+                <Skeleton height={30} width="80%" animation="wave" />
+                <Skeleton height={20} width="60%" animation="wave" />
+              </div>
+            </CardContent>
+            <CardActions className="d-flex justify-content-center">
+              <Skeleton height={35} width="60%" animation="wave" variant="rounded" />
+            </CardActions>
+          </Card>
+        )) :
+        instrumentList.map((instrument, index) => (
+          <Card key={index} elevation={0} className="instrument-card" sx={{ 
+          maxWidth: 345, 
+          backgroundColor: styles.colors.SECONDARY, 
+          paddingBottom: 2,
+          }}>
+            <CardMedia
+            sx={{ height: 160 }}
+            image="https://placehold.co/100"
+            title={instrument.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div" sx={{ fontFamily: styles.fonts.HEADER_FONT }}>
+                {instrument.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {instrument.family}
+              </Typography>
+            </CardContent>
+            <CardActions className="d-flex justify-content-center">
+              <Button size="small" 
+              variant="outlined" 
+              href={"/instruments/" + instrument.instrumentId}
+              disableElevation
+              color="success"
+              sx={{ textTransform: "none" }}
+              >
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+>>>>>>> Stashed changes
         ))}
       </div>
     </div>
