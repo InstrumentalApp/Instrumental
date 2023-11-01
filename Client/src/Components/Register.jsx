0eba47f2721579a4627695f46a59f025ca8f99f5
shu-william <williamshu@outlook.com>
2023-11-01 05:01:48 -0700

    minor formatting changes, registering now redirects home

--- Client/src/Components/Register.jsx
+++ Client/src/Components/Register.jsx
@@ -35,7 +35,7 @@ const Register = () => {
 		try {
 			const result = await axios.post("/api/auth/register", registerUser)
 			setCredentials(result.data);
-			navigate('/hello');
+			navigate('/');
 		} catch (err) {
 			const errorData = err.response.data.errors || err.response.data
 			const errorMessages = Object.values(errorData).flat();
@@ -47,23 +47,23 @@ const Register = () => {
 		<Container component="main" maxWidth="xs">
 			<CssBaseline />
 			<Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          backgroundColor: "white",
-          width: "360px",
-          padding: "40px 20px",
-          borderRadius: "10px",
-        }}
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					backgroundColor: "white",
+					width: "360px",
+					padding: "40px 20px",
+					borderRadius: "10px",
+				}}
 			>
-				<Typography variant='h5' sx={{ 
-					fontWeight: "bold", 
+				<Typography variant='h5' sx={{
+					fontWeight: "bold",
 					opacity: .8,
 					color: styles.colors.PRIMARY,
 					mb: 1,
-					}}
+				}}
 				>
 					Time to sign up
 				</Typography>
@@ -138,28 +138,29 @@ const Register = () => {
 						value={registerUser.confirm}
 						onChange={handleChange}
 					/>
-          <Button
-            type="submit"
-            color='warning'
-            disableElevation
-            variant="contained"
-            sx={{
+					<Button
+						type="submit"
+						color='warning'
+						disableElevation
+						variant="contained"
+						sx={{
 							width: "100%",
-              my: 1,
+							my: 1,
 							py: 1.1,
 							fontWeight: "bold",
-              textTransform: "none",
-            }}
-          >
-            Create account
-          </Button>
+							textTransform: "none",
+						}}
+					>
+						Create account
+					</Button>
 					<Grid container direction="column" alignItems="center" pt={1}>
 						<Grid item xs>
-							<Link component={RouterLink} to={"/sign-in"} underline="hover" variant="body2" 
-								sx={{ color: styles.colors.PRIMARY,
+							<Link component={RouterLink} to={"/sign-in"} underline="hover" variant="body2"
+								sx={{
+									color: styles.colors.PRIMARY,
 									fontWeight: "bold",
 									opacity: .7
-								}} 
+								}}
 							>
 								Already have an account? Sign in
 							</Link>
